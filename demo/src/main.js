449d78d7910e6c359e1cfb322e0ea14810229742
abdelouahed oumoussa <abdelwahd.oumouss@gmail.com>
2022-11-14 15:08:06 +0000

    fix(demo): fix style import for porduction build

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "../lib/style.css";
 
 import InfiniteLoading from "../../src/components/InfiniteLoading.vue";
 
@@ -8,6 +7,7 @@ const app = createApp(App);
 
 if (import.meta.env.MODE === "production") {
   const modules = import.meta.glob("../lib/v3-infinite-loading.mjs", { eager: true });
+  import.meta.glob("../lib/style.css", { eager: true })
   const InfiniteLoadingProd = modules["../lib/v3-infinite-loading.mjs"].default;
   app.component("infinite-loading", InfiniteLoadingProd);
 } else app.component("infinite-loading", InfiniteLoading);
