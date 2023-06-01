e3268653b664421754b5a6fb09fd3fa7cfaa3f6a
abdelouahed oumoussa <abdelwahd.oumouss@gmail.com>
2023-06-01 23:45:51 +0100

    update(demo): updated lib imports in main.js

--- demo/src/main.js
+++ demo/src/main.js
@@ -6,9 +6,9 @@ import InfiniteLoading from "../../src/components/InfiniteLoading.vue";
 const app = createApp(App);
 
 if (import.meta.env.MODE === "production") {
-  const modules = import.meta.glob("../lib/v3-infinite-loading.mjs", { eager: true });
+  const modules = import.meta.glob("../lib/v3-infinite-loading.js", { eager: true });
   import.meta.glob("../lib/style.css", { eager: true });
-  const InfiniteLoadingProd = modules["../lib/v3-infinite-loading.mjs"].default;
+  const InfiniteLoadingProd = modules["../lib/v3-infinite-loading.js"].default;
   app.component("InfiniteLoading", InfiniteLoadingProd);
 } else app.component("InfiniteLoading", InfiniteLoading);
 
